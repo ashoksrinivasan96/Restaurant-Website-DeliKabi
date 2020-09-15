c08782322946c56412fcd6d9fc1268a8d5748114
ashoksrinivasan96 <ashoksrinivasan96@gmail.com>
2020-09-15 22:26:06 +0530

    Changing corporate leadership

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -63,12 +63,10 @@ render(){
                         <i className="fa fa-envelope"></i>: <a href="mailto:delikabi@food.net">delikabi@food.net</a>
                     </address>
                 </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
+                
                 <div className="col-12 col-sm-11 offset-sm-1">
                     <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+8447878035"><i className="fa fa-phone"></i> Call</a>
+                        
                         <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
                         <a role="button" className="btn btn-success" href="mailto:delikabi@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                     </div>
